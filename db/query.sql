9df374ff7e7643cf0a8e6c54af2a8a0332c2895d
Durgavino <mailtodurga93@gmail.com>
2023-02-13 16:57:25 -0400

    added queries to the db

---
+++ db/query.sql
@@ -0,0 +1,10 @@
+Drop table userDetails;
+drop table Usersleepinfomation;
+select * from userDetails;
+select * from Usersleepinfomation;
+
+select * from summarys;
+
+
+
+select id,BedTime,wakeUpTime,(-(BedTime-12)+(0+wakeUpTime)) as Sleepdurtion from Usersleepinfomation;
